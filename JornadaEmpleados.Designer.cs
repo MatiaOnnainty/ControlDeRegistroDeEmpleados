1700793631e1ab51a5b311976c9f36938e8ac63a
Matias Onnainty <matiasonnainty@yahoo.com.ar>
2022-09-05 16:40:43 -0300

    Fucionando con rama maestra

--- JornadaEmpleados.Designer.cs
+++ JornadaEmpleados.Designer.cs
@@ -33,6 +33,8 @@
             this.labelNombre = new System.Windows.Forms.Label();
             this.labelDNI = new System.Windows.Forms.Label();
             this.labelHorasSemanales = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRegistros)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -49,7 +51,7 @@
             this.dataGridViewRegistros.ReadOnly = true;
             this.dataGridViewRegistros.RowHeadersVisible = false;
             this.dataGridViewRegistros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridViewRegistros.Size = new System.Drawing.Size(394, 331);
+            this.dataGridViewRegistros.Size = new System.Drawing.Size(394, 302);
             this.dataGridViewRegistros.TabIndex = 0;
             // 
             // pictureBox1
@@ -92,12 +94,38 @@
             this.labelHorasSemanales.TabIndex = 2;
             this.labelHorasSemanales.Text = "label1";
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(214, 446);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gold;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(110, 446);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Ver calendario";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
             // JornadaEmpleados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Chocolate;
             this.ClientSize = new System.Drawing.Size(418, 481);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.labelHorasSemanales);
             this.Controls.Add(this.labelDNI);
             this.Controls.Add(this.labelNombre);
@@ -124,5 +152,7 @@
         private System.Windows.Forms.Label labelNombre;
         private System.Windows.Forms.Label labelDNI;
         private System.Windows.Forms.Label labelHorasSemanales;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
